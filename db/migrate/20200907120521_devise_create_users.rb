7c3b8e77a0e473aad9070b56d7cc805d177499cc
keisuke-o <qiyoudaze5@gmail.com>
2020-09-08 12:11:56 +0900

    ユーザー認証機能の実装

--- db/migrate/20200907120521_devise_create_users.rb
+++ db/migrate/20200907120521_devise_create_users.rb
@@ -1,10 +1,10 @@
 # frozen_string_literal: true
 
-class AddDeviseToUsers < ActiveRecord::Migration[6.0]
-  def self.up
-    change_table :users do |t|
+class DeviseCreateUsers < ActiveRecord::Migration[6.0]
+  def change
+    create_table :users do |t|
       ## Database authenticatable
-      t.string :name,               null: false, unique: true, index: true
+      t.string :name, null: false
       t.string :email,              null: false, default: ""
       t.string :encrypted_password, null: false, default: ""
 
