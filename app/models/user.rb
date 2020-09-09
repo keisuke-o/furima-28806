b478733b8b5a006c8ae6fb27411149229989a706
keisuke-o <qiyoudaze5@gmail.com>
2020-09-09 16:35:04 +0900

    rspecのインストール

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,17 +4,14 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
   validates :name, presence: true, uniqueness: true
-  validates :pasword, presence :true, confirmation: true, format: { with: /\A[a-z0-9]+\z/ }, length: { minimum: 6 }
+  validates :pasword, presence: true, confirmation: true, format: { with: /\A[a-z0-9]+\z/ }, length: { minimum: 6 }
   validates :email, presence: true, uniqueness: true
   validates :nickname, presence: true
-  validates :family_name, presence: true, format{ with: /\A[ぁ-んァ-ン一-龥]/ }
-  validates :first_name, presence: true, format{ with: /\A[ぁ-んァ-ン一-龥]/ }
+  validates :family_name, presence: true, format: { with: /\A[ぁ-んァ-ン一-龥]/ }
+  validates :first_name, presence: true, format: { with: /\A[ぁ-んァ-ン一-龥]/ }
   validates :family_name_kana, presence: true, format: { with: /\A[ァ-ヶー－]+\z/ }
   validates :first_name_kana, presence: true, format: { with: /\A[ァ-ヶー－]+\z/ }
   validates :birth_date, presence: true
-
-
-
 
 end
 
--- db/schema.rb
+++ db/schema.rb
@@ -13,12 +13,20 @@
 ActiveRecord::Schema.define(version: 2020_09_07_120521) do
 
   create_table "users", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
+    t.string "nickname", null: false
+    t.string "name", null: false
     t.string "email", default: "", null: false
     t.string "encrypted_password", default: "", null: false
+    t.string "family_name", null: false
+    t.string "first_name", null: false
+    t.string "family_name_kana", null: false
+    t.string "first_name_kana", null: false
+    t.date "birth_date", null: false
     t.string "reset_password_token"
     t.datetime "reset_password_sent_at"
     t.datetime "remember_created_at"
     t.index ["email"], name: "index_users_on_email", unique: true
+    t.index ["name"], name: "index_users_on_name", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
