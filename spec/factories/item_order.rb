c867f7b088dc9209ea79d8ea095c52a29b44248e
keisuke-o <qiyoudaze5@gmail.com>
2020-09-22 21:28:26 +0900

    テストコードの記述

---
+++ spec/factories/item_order.rb
@@ -0,0 +1,10 @@
+FactoryBot.define do
+  factory :item_order do
+  prefecture_id    {3}
+  city          {"あああ"}
+  adress        {"あああ"}
+  zip_code      {"111-2222"}
+  telephone     {"01011112222"}
+  token         {11111}
+  end
+end
---
+++ spec/models/item_order_spec.rb
@@ -0,0 +1,46 @@
+require 'rails_helper'
+
+RSpec.describe ItemOrder, type: :model do
+  before do
+    @item_order = FactoryBot.build(:item_order)
+  end
+
+  describe '商品購入機能' do
+    it "クレジットカード情報が正しく入力されていないと購入できない" do
+      @item_order.token = ""
+      @item_order.valid?
+      expect(@item_order.errors.full_messages).to include("Token can't be blank")
+    end
+
+    it "配送先の地域が'--'だと登録できない" do
+      @item_order.prefecture_id = ""
+      @item_order.valid?
+      expect(@item_order.errors.full_messages).to include("Prefecture is not a number")
+    end
+
+    it "住所が正しく入力されていないと購入できない" do
+      @item_order.adress = ""
+      @item_order.valid?
+      expect(@item_order.errors.full_messages).to include("Adress can't be blank")
+    end
+
+    it "郵便番号が正しく入力されていないと購入できない" do
+      @item_order.zip_code = ""
+      @item_order.valid?
+      expect(@item_order.errors.full_messages).to include("Zip code can't be blank", "Zip code is invalid")
+    end
+
+    it "電話番号が正しく入力されていないと購入できない" do
+      @item_order.telephone = ""
+      @item_order.valid?
+      expect(@item_order.errors.full_messages).to include("Telephone can't be blank", "Telephone is invalid")
+    end
+  end
+
+  describe '商品購入機能' do
+    it "全て入力されていれば購入できる" do
+      expect(@item_order).to be_valid
+    end
+  end
+
+end
