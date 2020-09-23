0378434f354e9503af2b674c17b4868352835fec
keisuke-o <qiyoudaze5@gmail.com>
2020-09-23 12:35:55 +0900

    異常系テストコードの追加

--- spec/models/item_order_spec.rb
+++ spec/models/item_order_spec.rb
@@ -24,6 +24,12 @@ RSpec.describe ItemOrder, type: :model do
       expect(@item_order.errors.full_messages).to include("Adress can't be blank")
     end
 
+    it "郵便番号に'-'が含まれていないと購入できない" do
+      @item_order.zip_code = 3334444
+      @item_order.valid?
+      expect(@item_order.errors.full_messages).to include("Zip code is invalid")
+    end
+
     it "郵便番号が正しく入力されていないと購入できない" do
       @item_order.zip_code = ""
       @item_order.valid?
@@ -34,6 +40,12 @@ RSpec.describe ItemOrder, type: :model do
       @item_order.telephone = ""
       @item_order.valid?
       expect(@item_order.errors.full_messages).to include("Telephone can't be blank", "Telephone is invalid")
+    end
+
+    it "電話番号に'-'が入力されていると購入できない" do
+      @item_order.telephone = 010-2222-3333
+      @item_order.valid?
+      expect(@item_order.errors.full_messages).to include("Telephone is invalid")
     end
   end
 
